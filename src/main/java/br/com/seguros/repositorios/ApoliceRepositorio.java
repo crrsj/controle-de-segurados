e58462bdbdc490792ab8b693ba884c9ffeb57b43
crrsj <crrsj1@gmail.com>
2026-06-27 12:05:18 -0300

    removendo duplicidade no repositorio

--- src/main/java/br/com/seguros/repositorios/ApoliceRepositorio.java
+++
@@ -1,7 +0,0 @@
-package br.com.seguros.repositorios;
-
-import br.com.seguros.entidades.Apolice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApoliceRepositorio extends JpaRepository<Apolice,Long> {
-}
--- src/main/java/br/com/seguros/repositorios/SeguradoRepositorio.java
+++
@@ -1,7 +0,0 @@
-package br.com.seguros.repositorios;
-
-import br.com.seguros.entidades.Segurado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeguradoRepositorio extends JpaRepository<Segurado,Long> {
-}
