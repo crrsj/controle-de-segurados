82ba8793f8badb718699c945b32d92ec3f3a990e
crrsj <crrsj1@gmail.com>
2026-06-27 20:15:53 -0300

    Criando o metodo de salvar apolice

--- src/main/java/br/com/seguros/servico/ApoliceServico.java
+++ src/main/java/br/com/seguros/servico/ApoliceServico.java
@@ -1,9 +1,31 @@
 package br.com.seguros.servico;
 
+import br.com.seguros.dtos.ApoliceDto;
+import br.com.seguros.entidades.Apolice;
+import br.com.seguros.excecoes.ExcecaoSeguradoNaoEncontrado;
+import br.com.seguros.repositorio.ApoliceRepositorio;
+import br.com.seguros.repositorio.SeguradoRepositorio;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ApoliceServico {
+
+    private final ApoliceRepositorio apoliceRepositorio;
+    private final SeguradoRepositorio seguradoRepositorio;
+    private final ModelMapper modelMapper;
+
+
+    @Transactional
+    public ApoliceDto salvarApolice(Long seguradoId,ApoliceDto apoliceDto){
+        var segurado = seguradoRepositorio.findById(seguradoId).orElseThrow(()->
+                new ExcecaoSeguradoNaoEncontrado("Segurado não localizado !"));
+        var apolice = modelMapper.map(apoliceDto, Apolice.class);
+        apolice.setSegurado(segurado);
+       var novaApolice = apoliceRepositorio.save(apolice);
+       return modelMapper.map(novaApolice, ApoliceDto.class);
+    }
 }
