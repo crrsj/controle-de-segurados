b7b7f67191d34a328497b5195339cdd7534a7cd2
crrsj <crrsj1@gmail.com>
2026-06-27 17:04:28 -0300

    Adicionando metodo para buscar o segurado pelo cpf

--- src/main/java/br/com/seguros/repositorio/SeguradoRepositorio.java
+++ src/main/java/br/com/seguros/repositorio/SeguradoRepositorio.java
@@ -1,7 +1,12 @@
 package br.com.seguros.repositorio;
 
+import br.com.seguros.dtos.SeguradoDto;
+import br.com.seguros.dtos.SeguradoSemCpf;
 import br.com.seguros.entidades.Segurado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SeguradoRepositorio extends JpaRepository<Segurado,Long> {
+    Optional<Segurado> findByCpf(String cpf);
 }
