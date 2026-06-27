f435c685a526b4f330ee0fe00be612426142ed25
crrsj <crrsj1@gmail.com>
2026-06-27 01:02:52 -0300

    Adicionando o Openapi-swagger

--- src/main/java/br/com/seguros/SegurosApplication.java
+++ src/main/java/br/com/seguros/SegurosApplication.java
@@ -1,9 +1,20 @@
 package br.com.seguros;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API - Controle De Segurados",
+				version = "1.0",
+				description = " Sistema de gestão para serviços de turismo",
+				contact = @Contact()
+		)
+)
 public class SegurosApplication {
 
 	public static void main(String[] args) {
