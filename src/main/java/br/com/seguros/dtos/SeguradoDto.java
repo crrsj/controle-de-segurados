75be8940995f21960e8abaf86cc0c1023616f449
crrsj <crrsj1@gmail.com>
2026-06-27 11:37:13 -0300

    criando dtos

---
+++ src/main/java/br/com/seguros/dtos/SeguradoDto.java
@@ -0,0 +1,18 @@
+package br.com.seguros.dtos;
+
+import br.com.seguros.entidades.Apolice;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class SeguradoDto {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private List<Apolice> apolices = new ArrayList<>();
+}
